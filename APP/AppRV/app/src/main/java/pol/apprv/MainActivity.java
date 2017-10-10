c95613d710625e834276ed839d16022c64cb9283
Ariel Ortega <26ariel.ortega@gmail.com>
2017-10-10 00:23:14 -0300

    Implementando la comunicación con el servicio para poder validar el usuario que ingresa.

--- APP/AppRV/app/src/main/java/pol/apprv/MainActivity.java
+++ APP/AppRV/app/src/main/java/pol/apprv/MainActivity.java
@@ -1,9 +1,11 @@
 package pol.apprv;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,6 +23,9 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
+import org.apache.http.client.HttpClient;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private LinearLayout photoSeccion;
@@ -30,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView photoPie;
     private GoogleApiClient googleApiClient;
     private static final int REQ_CODE = 9001;
+
+    //AQUI HAY QUE CAMBIAR LA IP Y EL PUERTO DONDE SE EJECUTA EL WEB SERVICE
+    public static final String servidor="http://192.168.1.72:8084";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +92,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             GoogleSignInAccount account = result.getSignInAccount();
             String name = account.getDisplayName();
             String correo = account.getEmail();
+
             String imagenURL = account.getPhotoUrl().toString();
             nombre.setText(name);
             email.setText(correo);
@@ -111,6 +120,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (requestCode==REQ_CODE){
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleResult(result);
+        }
+    }
+
+    private class isUser extends AsyncTask <String,Integer,Boolean> {
+        static final String linkServicio = servidor+"/ServicioRV/webresources/usuario/isuser";
+        private String id_usuario;
+        protected Boolean doInBackground(String... params) {
+            JSONObject jsonParam = new JSONObject();
+            boolean resultado = false;
+            try {
+                jsonParam.put("correo", email);
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost del = new HttpPost(linkService);
+                del.setHeader("Accept", "application/json");
+                del.setHeader("Content-type", "application/json");
+                StringEntity se = new StringEntity(jsonParam.toString());
+                del.setEntity(se);
+                //
+                HttpResponse resp = httpClient.execute(del);
+                String respStr = EntityUtils.toString(resp.getEntity());
+                JSONObject obj = new JSONObject(respStr);
+                if (obj != null) {
+                    id_usuario = obj.getString("id_usuario");
+                    resultado = true;
+                }
+                else {
+                    resultado = false;
+                }
+            } catch (Exception ex) {
+                Log.e("ServicioRest", "Error!", ex);
+            }
+            return resultado;
         }
     }
 }
